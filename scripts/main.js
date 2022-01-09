8ef89eb680819dd60fe8849e9df841afe5c21411
Kendrick McDonald <kendrickmcdonald93@gmail.com>
2022-01-09 16:40:22 -0500

    move content to beginner page and update styles

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-function showtext {
-	var explainer = document.getElementById('definition')
-	if (x.style.display === 'none') {
-		x.style.display = "block";
-	}
-	else {
-		x.style.display = "none";
-	}
-}
